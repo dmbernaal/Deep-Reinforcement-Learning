1f921bb4a9ca7532bc509425ef2ca68ba8a235d4
Diego-Medina-Bernal <dmbernaal@gmail.com>
2019-03-31 14:56:12 -0400

    Initial Config file

---
+++ config.sh
@@ -0,0 +1,21 @@
+# Configuration file: Instaling dependencies & Conda Env
+
+# installing all dependencies
+pip install numpy==1.14.2
+pip install atari-py==0.1.1
+pip install gym==0.10.4
+pip install ptan==0.3
+pip install opencv-python==3.4.0.12
+pip install torch 
+pip install fastai
+pip install torchvision
+pip install tensorboard-pytorch==0.7.1
+pip install tensorflow==1.7.0
+pip instsll tensorboard==1.7.0
+pip install tqdm
+
+echo "Finished Intalling!"
+echo "Ready to run any agents"
+echo "When you finish run:" 
+echo "deactive" 
+echo "on terminal"
